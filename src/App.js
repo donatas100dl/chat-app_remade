fe20797ebe404e57ed31a399b585fb0dbe3e9c6b
donatas100dl <77070458+donatas100dl@users.noreply.github.com>
2023-09-28 11:15:48 +0300

    class job

--- src/App.js
+++ src/App.js
@@ -4,11 +4,42 @@ import PrivateRoutes from "./uttils/pivateRoutes";
 import Dashboard from "./pages/dashboard.js";
 import Login from "./pages/login";
 import { AuthProvider } from "./uttils/authContext";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+const socket = io("http://localhost:4000/");
+
+
+
+
+
 function App() {
+
+  socket.on("connect", () => {
+    console.log(socket.id);
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    socket.emit("sendMessage", `hello world im client: ${socket.id}`,logMessage)
+  }
+
+  const logMessage = (msg) => {
+    console.log(msg)
+  }
+  
+  useEffect(() => {
+  socket.on("brodcastMessage", (data) => {
+    console.log(data)
+  })
+
+  }, [socket])
+
   return (
     <div className="App">
+      <button onClick={(e ) => handleClick(e)}>PRess meeeee</button>
         <AuthProvider>
           <Routes>
+
             <Route path="/user/login" element={<Login />} />
 
             <Route element={<PrivateRoutes />}>
